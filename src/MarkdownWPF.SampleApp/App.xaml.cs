28c251d80178000bbf5021f7f33f15a5558cc018
IgorShaposhnikov <Igor.Shaposhnikov@urfu.me>
2025-10-23 18:07:32 +0500

    Add MarkdownWPFModule with method for registration dictionary with Markdown Region DataTemplates

--- src/MarkdownWPF.SampleApp/App.xaml.cs
+++ src/MarkdownWPF.SampleApp/App.xaml.cs
@@ -1,12 +1,25 @@
 ﻿using System.Windows;
 
-namespace MarkdownWPF.App
+namespace MarkdownWPF.SampleApp
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
 
+            // Add default template
+
+            MarkdownWPFModule.RegisterBlockTemplates(
+                Current.Resources
+                );
+
+            MarkdownWPFModule.RegisterCustomTemplates(
+                Current.Resources, "pack://application:,,,/MarkdownWPF.SampleApp;component/MVVM/Views/MarkdownElementDataTemplates.xaml"
+            );
+        }
     }
 }
---
+++ src/MarkdownWPF/MarkdownWPFModule.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace MarkdownWPF
+{
+    public static class MarkdownWPFModule
+    {
+        /// <summary>
+        /// Add to app resource dictionary implementation of datatemplates
+        /// </summary>
+        /// <param name="resourceDictionary">Current app resource dictionary</param>
+        /// <param name="path">Custom path to resource dictionary with datatemplates</param>
+        public static void RegisterBlockTemplates(this ResourceDictionary resourceDictionary, string path = null)
+        {
+            path = string.IsNullOrEmpty(path)
+                ? "pack://application:,,,/MarkdownWPF;component/MarkdownElementDataTemplates.xaml"
+                : path;
+
+            resourceDictionary.MergedDictionaries.Add(new ResourceDictionary()
+            {
+                Source = new Uri(path, UriKind.RelativeOrAbsolute)
+            });
+        }
+
+        public static void RegisterCustomTemplates(this ResourceDictionary resourceDictionary, string path = null) 
+        {
+            resourceDictionary.MergedDictionaries.Add(new ResourceDictionary()
+            {
+                Source = new Uri(path, UriKind.RelativeOrAbsolute)
+            });
+        }
+    }
+}
