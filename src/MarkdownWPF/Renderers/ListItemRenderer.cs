a9def49882aefb317fab134b6b7e41539bff6a1d
IgorShaposhnikov <Igor.Shaposhnikov@urfu.me>
2026-03-07 14:03:57 +0500

    Fix numeric list display issue

--- src/MarkdownWPF/Renderers/ListItemRenderer.cs
+++ src/MarkdownWPF/Renderers/ListItemRenderer.cs
@@ -2,25 +2,43 @@ using Markdig.Renderers;
 using Markdig.Syntax;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Documents; // Нужно для TextElement
 
 namespace MarkdownWPF.Renderers
 {
     public class ListItemRenderer : MarkdownObjectRenderer<WpfVirtualizingRenderer, ListItemBlock>
     {
-        protected override void Write(WpfVirtualizingRenderer renderer, ListItemBlock obj)
+        protected override void Write(WpfVirtualizingRenderer renderer, ListItemBlock listItemBlock)
         {
-            var bulletPanel = new StackPanel { Orientation = Orientation.Horizontal, Margin = new Thickness(0,0,0,5) };
-            var bullet = new TextBlock { Text = "•  ", FontWeight = FontWeights.Bold, FontSize = 16 };
-            bulletPanel.Children.Add(bullet);
+            var stackPanel = new StackPanel { Orientation = Orientation.Horizontal };
 
-            var contentPanel = new StackPanel { VerticalAlignment = VerticalAlignment.Center };
-            bulletPanel.Children.Add(contentPanel);
+            string markerText = "• ";
+            if (renderer.CurrentContext is StackPanel parentList && parentList.Tag is int orderNumber && orderNumber != -1)
+            {
+                char delimiter = ((ListBlock)listItemBlock.Parent).OrderedDelimiter;
+                markerText = $"{orderNumber}{delimiter} ";
+            }
 
-            if (renderer.CurrentContext is StackPanel parentSp) parentSp.Children.Add(bulletPanel);
+            var marker = new TextBlock
+            {
+                Text = markerText,
+                Margin = new Thickness(0, 0, 5, 0)
+            };
+
+            var contentPanel = new StackPanel();
+            contentPanel.VerticalAlignment = VerticalAlignment.Center;
+
+            stackPanel.Children.Add(marker);
+            stackPanel.Children.Add(contentPanel);
 
             renderer.Push(contentPanel);
-            renderer.WriteChildren(obj);
+            renderer.WriteChildren(listItemBlock);
             renderer.Pop();
+
+            if (renderer.CurrentContext is Panel parentPanel)
+            {
+                parentPanel.Children.Add(stackPanel);
+            }
         }
     }
 }
--- src/MarkdownWPF/Renderers/ListRenderer.cs
+++ src/MarkdownWPF/Renderers/ListRenderer.cs
@@ -7,15 +7,43 @@ namespace MarkdownWPF.Renderers
 {
     public class ListRenderer : MarkdownObjectRenderer<WpfVirtualizingRenderer, ListBlock>
     {
-        protected override void Write(WpfVirtualizingRenderer renderer, ListBlock obj)
+        protected override void Write(WpfVirtualizingRenderer renderer, ListBlock listBlock)
         {
-            var stackPanel = new StackPanel { Margin = new Thickness(15, 0, 0, 10) };
-            if (renderer.CurrentContext == null) renderer.RootElements.Add(stackPanel);
-            else if (renderer.CurrentContext is StackPanel sp) sp.Children.Add(stackPanel);
+            var stackPanel = new StackPanel
+            {
+                Margin = new Thickness(0, 5, 0, 5)
+            };
+
+            int orderStart = 1;
+            if (listBlock.IsOrdered && int.TryParse(listBlock.OrderedStart, out int start))
+            {
+                orderStart = start;
+            }
+
+            stackPanel.Tag = listBlock.IsOrdered ? orderStart : -1;
 
             renderer.Push(stackPanel);
-            renderer.WriteChildren(obj);
+
+            foreach (var item in listBlock)
+            {
+                renderer.Write(item);
+
+                if (listBlock.IsOrdered && stackPanel.Tag is int currentOrder)
+                {
+                    stackPanel.Tag = currentOrder + 1;
+                }
+            }
+
             renderer.Pop();
+
+            if (renderer.CurrentContext is Panel parentPanel)
+            {
+                parentPanel.Children.Add(stackPanel);
+            }
+            else
+            {
+                renderer.RootElements.Add(stackPanel);
+            }
         }
     }
 }
